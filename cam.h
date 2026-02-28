d520efcfbe87fbe310bc5d180eccbe1b518c3414
gnom2949 <sasasilaev@gmail.com>
2026-02-28 17:16:48 +0500

    Added new animation, bruteforce function, and base64 encode(created by AI)

--- cam.h
+++ cam.h
@@ -1,6 +1,6 @@
 /* Cam.h v1
  * The header for cameradar written on C
- * No license
+ * 
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +17,8 @@
 #include <string.h>
 #include <curl/curl.h>
 #include <json-c/json.h>
+#include <stdint.h>
+#include <stdatomic.h>
 
 #ifndef C_CAM_RADAR_H
 #define C_CAM_RADAR_H
@@ -36,6 +38,8 @@ typedef struct {
   double resT;
   int timeout_ms;
   char service[32];
+  bool doBrute;
+  bool doVuln;
 } radarType;
 
 typedef struct {
@@ -49,17 +53,36 @@ typedef struct {
   char pass[128];
 } BrutePair;
 
-typedef struct {
-  char id[64];
-  char secret[64];
-} censysAuth;
+struct MemBuf {
+  char *memory;
+  size_t size;
+};
 
+typedef struct {
+  char msg[256];
+  atomic_bool active;
+}aConf;
+
+aConf globA = {"Initializing...", true};
 int spawnSock (radarType* target);
 bool portCheck (radarType* target);
 char* ipToString (radarType* target);
-void scanim (void);
+void* scanim (void* arg);
+static size_t WrMemCallback (void *contents, size_t size, size_t nmemb, void *userp);
 bool check4Cam (int sockfd, radarType* target);
-bool loadCensysKey (censysAuth* auth, radarType* target);
+bool loadCensysKey (radarType* target);
 bool bruteforce (radarType* target);
+void parseJson (const char *jString);
+void bsfEncode (const char* input, char* output);
+
+const char* rtspPath[] = {
+  "/live/ch0", "/live/ch1", // Hikvision or Generic
+  "/cam/realmonitor?channel=1&subtype=0", //Dahua
+  "/Streaming/Channels/101", // ONVIF
+  "/mpeg4", "/h264", // Chinese cameras
+  "/ipcam/stream.asf", // Specific vendors
+  "/videoMain", // XMeye
+  "/" // root 
+};
 
 #endif
--- main.c
+++ main.c
@@ -5,96 +5,131 @@
 
 #include "cam.h"
 
-bool loadCensysKey (censysAuth* auth, radarType* target)
+static size_t WrMemCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-  FILE *f = fopen ("censys.api", "r");
-  if (!f) {
-    fprintf (stderr, COL_RED "\n[ERROR] File censys.api not found\n" COL_DEF);
-    return false;
-  }
-  char token[128];
-  if (fscanf (f, "%127s", token) != 1) {
-    fclose (f); return false;
-  }
-  fclose (f);
+    if (!contents || !userp) return 0;
+    struct MemBuf *mem = userp;
+    if (nmemb != 0 && size > SIZE_MAX / nmemb) return 0;
+
+    size_t realsize = size * nmemb;
+
+    if (mem->size > SIZE_MAX - realsize - 1) return 0;
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if (!ptr) return 0;
+
+    mem->memory = ptr;
+
+    memcpy(mem->memory + mem->size, contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = '\0';
+
+    return realsize;
+}
+
+void bsfEncode (const char* input, char* output)
+{
+  const char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  int i = 0, j = 0;
+  int len = strnlen (input, (sizeof input));
+  unsigned char tmp[3];
   
-  CURL *curl = curl_easy_init ();
-  if (curl) {
-    char url[256];
-    char auth_header[256];
-    struct in_addr ipStruct;
-    ipStruct.s_addr = target->ipAddr;
-    
-    snprintf (url, sizeof (url), "https://api.platform.censys.io/%s", inet_ntoa (ipStruct));
-    snprintf (auth_header, sizeof (auth_header), "Authorization: Bearer %s", token);
-    
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append (headers, "Accept: application/vnd.censys.api.v3.host.v1+json");
-    headers = curl_slist_append (headers, auth_header);
-    
-    curl_easy_setopt (curl, CURLOPT_URL, url);
-    curl_easy_setopt (curl, CURLOPT_HTTPHEADER, headers);
-    curl_easy_setopt (curl, CURLOPT_TIMEOUT, 10L);
-    
-    printf (COL_CYAN "[INFO] Do a query to the censys api for %s\n" COL_DEF, inet_ntoa (ipStruct));
-    
-    CURLcode res = curl_easy_perform (curl);
-    if (res != CURLE_OK) {
-      fprintf (stderr, COL_RED "[ERROR] Curl output: %s\n" COL_DEF, curl_easy_strerror (res));
+  while (len--) {
+    tmp[i++] = *(input++);
+    if (i == 3) {
+      output[j++] = table[tmp[0] >> 2];
+      output[j++] = table[((tmp[0] & 0x03) << 4) | (tmp[1] >> 4)];
+      output[j++] = table[((tmp[1] & 0x0f) << 2) | (tmp[2] >> 6)];
+      output[j++] = table[tmp[2] & 0x03f];
+      i = 0;
     }
-              
-    curl_slist_free_all (headers);
-    curl_easy_cleanup (curl);
   }
+  
+  if (i) {
+    output[j++] = table[tmp[0] >> 2];
+    if (i == 1) {
+      output[j++] = table[(tmp[0]  & 0x03) << 4];
+      output[j++] = '=';    
+    } else {
+      output[j++] = table[((tmp[0] & 0x03) << 4) | (tmp[1] >> 4)];
+      output[j++] = table[(tmp[1] & 0x0f) << 2];
+    }
+    output[j++] = '=';
+  }
+  output[j] = '\0';
 }
 
 bool bruteforce (radarType* target)
 {
+  pthread_t anim_tid;
+  atomic_store (&globA.active, true);
   FILE *fLog = fopen ("brute/logins.txt", "r");
   FILE *fPass = fopen ("brute/passwords.txt", "r");
+  pthread_create (&anim_tid, NULL, scanim, &globA);
   
   if (!fLog || !fPass) {
     fprintf (stderr, COL_RED "\n[ERROR] Keywords not found in brute dir!!\n" COL_DEF);
+    if (fLog) fclose (fLog); if (fPass) fclose (fPass);
     return false;
   }
   
-  char login[256], pass[256]; 
-  while (fgets(login, sizeof(login), fLog)) {
-        login[strcspn(login, "\n")] = 0; 
+  char login[128], pass[128];
+  char rawAu[128], bsfAu[256];
+  char req[1024], resp[1024];
+  char *ip = inet_ntoa (*(struct in_addr*)&target->ipAddr);
+  
+  for (int p = 0; p < 5; p++) {
+    rewind (fLog);
+    while (fgets (login, sizeof (login), fLog)) {
+      login[strcspn (login, "\r\n")] = 0;
+      
+      rewind (fPass);
+      while (fgets (pass, sizeof (pass), fPass)) {
+        pass[strcspn (pass, "\r\n")] = 0;
         
-        rewind(fPass); 
-          while (fgets(pass, sizeof(pass), fPass)) {
-            pass[strcspn(pass, "\n")] = 0;
-
-            int fd = spawnSock(target);
-            if (fd < 0) continue;
-
-            char request[512];
-            snprintf(request, sizeof(request),
-                     "DESCRIBE rtsp://%s:%s@%s:%d/ RTSP/1.0\r\n"
-                     "CSeq: 1\r\n\r\n", 
-                     login, pass, inet_ntoa(*(struct in_addr*)&target->ipAddr), target->port);
-
-            send(fd, request, strlen(request), 0);
-            char response[1024];
-            int received = recv(fd, response, sizeof(response)-1, 0);
-            close(fd);
-
-            if (received > 0) {
-                response[received] = '\0';  
-                if (strstr(response, "RTSP/1.0 200 OK")) {
-                    printf(COL_GRN "[SUCCESS] Found: %s:%s\n" COL_DEF, login, pass);
-                    fclose(fLog); fclose(fPass);
-                    return true;
-                }
-            }
-            printf("\r[BRUTE] Trying %s:%s...  ", login, pass);
-            fflush(stdout);
+        int fd = spawnSock (target);
+        if (fd < 0) usleep (500000); continue;
+        
+        // prepare a header in base64
+        snprintf (rawAu, sizeof (rawAu), "%s:%s", login, pass);
+        bsfEncode (rawAu, bsfAu);
+        
+        // describe
+        snprintf (req, sizeof (req),
+                  "DESCRIBE rtsp://%s:%d%s RTSP/1.0\r\n"
+                  "CSeq: 1\r\n"
+                  "Authorization: Basic %s\r\n"
+                  "User-Agent: SatWatcher/1.0\r\n\r\n",
+                  ip, target->port, rtspPath[p], bsfAu);
+        
+        send (fd, req, strnlen (req, sizeof req), 0);
+        
+        memset (resp, 0, sizeof (resp));
+        int received = recv (fd, resp, sizeof(resp) - 1, 0);
+        close (fd);
+        
+        if (received > 0) {
+          if (strstr (resp, "RTSP/1.0 200")) {
+            printf (COL_GRN "\n[SUCCESS] %s:%d%s -> %s:%s\n" COL_DEF, ip, target->port, rtspPath[p], login, pass);
+            FILE *res = fopen ("found.txt", "a");
+            fprintf (res, "rtsp://%s:%s@%s:%d%s\n", login, pass, ip, target->port, rtspPath[p]);
+            fclose (res);
+            
+            fclose (fLog); fclose (fPass);
+            return true;
+          }
         }
+        printf (globA.msg, 256, COL_BLU "\r[INFO] Brute %s:%s on %s, please wait." COL_DEF, login, pass, rtspPath[p]);
+        fflush (stdout);
+        
+        usleep (100000);
+      }
     }
-    
-    fclose(fLog); fclose(fPass);
-    return false;
+  } 
+  atomic_store (&globA.active, false);
+  pthread_join (anim_tid, NULL);
+  fclose(fLog);
+  fclose(fPass);
+  return false;
 }
 
 int spawnSock (radarType* target)
@@ -123,55 +158,61 @@ int spawnSock (radarType* target)
   }
   
   target->is_open = true;
+  printf (COL_GRN "\n[SUCC]Socket spawned\n" COL_DEF);
   return sockfd;
 }
 
-bool check4Cam (int sockfd, radarType* target)
+bool check4Cam (int sockfd, radarType* target) 
 {
   char buf[1024];
-  char req[256];
+  char req[512];
+  char ipStr[INET_ADDRSTRLEN];
+  strncpy (ipStr, inet_ntoa (*(struct in_addr*)&target->ipAddr), INET_ADDRSTRLEN);
   
-  snprintf (req, sizeof (req),
-            "DESCRIBE rtsp://%s:%d/ RTSP/1.0\r\n"
-            "CSeq: 1\r\n\r\n",  
-            inet_ntoa (*(struct in_addr*)&target->ipAddr), target->port);
-  
-  if (send(sockfd, req, strnlen (req, sizeof (req)), 0) < 0) {  
-       return false;
-  }
-  
-  ssize_t received = recv (sockfd, buf, sizeof (buf) - 1, 0); 
-  if (received > 0) {
-    buf[received] = '\0';
+  for (int i = 0; i < (sizeof (rtspPath)/sizeof(char*)); i++) {
+    snprintf (req, sizeof (req),
+              "DESCRIBE rtsp://%s:%d%s RTSP/1.0\r\n"
+              "CSeq: 1\r\n"
+              "User-Agent: SatWatcher/1.0\r\n"
+              "Accept: application/sdp\r\n\r\n",          
+              ipStr, target->port, rtspPath[i]);
     
-    if (strstr (buf, "RTSP/1.0") != NULL) {
-      char *server_head = strstr (buf, "Server:");
-      if (server_head) {
-        snprintf (target->service, sizeof (target->service), "RTSP (%.20s)", server_head + 8);        
-      } else {
-        snprintf (target->service, sizeof (target->service), "RTSP"); 
+    send (sockfd, req, strlen (req), 0);
+    memset (buf, 0, sizeof (buf));
+    ssize_t received = recv (sockfd, buf, sizeof (buf) - 1, 0);
+    
+    if (received > 0) {
+      
+      if (strstr (buf, "RTSP/1.0") != NULL) {
+        
+        if (strstr (buf, "200 OK") != NULL) {
+          printf (COL_GRN "\n[SUCCESS] Found opened stream: rtsp://%s:%d%s\n" COL_DEF, ipStr, target->port, rtspPath[i]);
+          snprintf (target->service, sizeof (target->service), "RTSP OPEN");
+          return true;  
+        }
+        
+        if (strstr (buf, "401") != NULL || strstr (buf, "Unathorized") != NULL) {
+          printf (COL_YLW "\n[WARNING] Path Found: %s, needed login!\n" COL_DEF, rtspPath[i]);
+          snprintf (target->service, sizeof (target->service), "RTSP AUTH REQUIRED!", rtspPath[i]);
+          return true;
+        }                                     
       }
-      return true;
     }
   }
   return false;
 }
 
-void scanim (void)
+void* scanim (void* arg)
 {
-  const char* erp[] = {"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"};
-  int frameof = 0;
-  int num_frames = 10;
-  
-  for (int i = 0; i < 40; i++) { 
-    printf ("\rScanning... %s", erp[frameof]);
-    
+  aConf* cfg = (aConf*)arg;
+  const char* frames[] = {"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"};
+  int i = 0;
+  while (atomic_load (&cfg->active)) {
+    printf ("\r\33[2K" COL_PRPL "[%s] %s" COL_DEF, frames[i++ % 10], cfg->msg);
     fflush (stdout);
-    frameof = (frameof + 1) % num_frames;
-    
-    usleep (100000);
+    usleep (80000);
   }
-  printf("\n");
+  return NULL;
 }
 
 void usage (void)
@@ -181,52 +222,52 @@ void usage (void)
   printf ("\n       -t value(ip)\n");
   printf ("\n       -p value, you can type an any port that needs to scan for RTSP\n");
   printf ("\n       -b with this you can do bruteforce, reading the logins and passwords text file in brute dir\n");
-  printf ("\n       -aV with this you can do query to censys, needs token from censys and put is to censys.api file\n");
   printf ("\n Github: https://github.com/gnom2949/cameradarC\n");
   printf ("\n Credits: https://github.com/Ullaakut/cameradar\n");
   printf ("\nThis is a free software under GNU GPLv3, see more: https://www.gnu.org/licenses/gpl-3.0.html\n");
   exit (1);
 }
 
-void* threadScan (void* arg)
-{
-  // copying memory to avoid TOCTOU (race condition)
-  radarType* data = (radarType*)arg;
-  
-  int s = spawnSock (data);
-  if (s >= 0) {
-    printf (COL_GRN "\n[INFO] Port %d is OPEN\n" COL_DEF, data->port);  
+void* threadScan(void* arg) {
+    radarType* data = (radarType*)arg;
     
-    if (check4Cam (s, data)) {
-      printf (COL_CYAN "\n[INFO] RTSP CAMERA FOUND\n" COL_DEF);
+    int fd = spawnSock(data);
+    if (fd >= 0) {
+        printf(COL_GRN "\n[INFO] Port %d is OPEN\n" COL_DEF, data->port);
+        
+        if (check4Cam (fd, data)) {
+            if (data->doBrute) {
+                bruteforce(data);
+            }
+        }
+        close(fd);
     }
-    close (s);
-  }
-  
-  free (data);
-  return NULL;
+    
+    free(data);
+    return NULL;
 }
+
 
 int main (int argc, char *argv[])
 {
+  pthread_t anim_tid;
   radarType target = {0};
-  censysAuth auth = {0};
+  atomic_store (&globA.active, true);
   target.timeout_ms = 1000;
+  pthread_create (&anim_tid, NULL, scanim, &globA);
   
   static struct option ion[] = {
     {"target", required_argument, 0, 't'},
     {"port", required_argument, 0, 'p'},
     {"brute", no_argument, 0, 'b'},
-    {"all", no_argument, 0, 'a'},
-    {"vulnerability", no_argument, 0, 'V'},
     {"help", no_argument, 0, 'h'},
     {0, 0, 0, 0}
   };
   
   int opt, optIndex = 0, portSt = 0, portEnd = 0;
-  int bruteF = 0, allF = 0, vulnF = 0;
+  int bruteF = 0;
    
-  while ((opt = getopt_long (argc, argv, "t:p:", ion, &optIndex)) != -1) {
+  while ((opt = getopt_long (argc, argv, "t:p:bh", ion, &optIndex)) != -1) {
     switch (opt) {
       case 't':
         target.ipAddr = inet_addr (optarg);
@@ -239,22 +280,9 @@ int main (int argc, char *argv[])
         }
         break;
       case 'b':
-        if (bruteforce (&target)) {
-          printf (COL_CYAN "\n[INFO] Starting brute force\n" COL_DEF);
-          scanim ();
-          if (!bruteforce (&target)) {
-            fprintf (stderr, COL_RED "\n[FAILURE] No valid credentials found!\n" COL_DEF);
-            exit (1);
-          }
-        }
+        bruteF = 1;
         break;
       case 'h':
-        usage();
-        return 0;
-        break;
-      case 'a':
-        allF = 1;
-        break;
       default:
         usage();
         return 1;
@@ -264,10 +292,9 @@ int main (int argc, char *argv[])
   if (target.ipAddr == INADDR_NONE || portSt == 0) { 
     usage();
     return 1;
-  }
+  } 
   
-  printf (COL_CYAN "\n[INFO] Scanning target: %s\n" COL_DEF, inet_ntoa (*(struct in_addr*)&target.ipAddr));
-  scanim ();
+  printf (globA.msg, 256,COL_CYAN "\n[INFO] Scanning target: %s\n" COL_DEF, inet_ntoa (*(struct in_addr*)&target.ipAddr));
   
   pthread_t threads[portEnd - portSt + 1];
   int threadCount = 0;
@@ -289,6 +316,10 @@ int main (int argc, char *argv[])
     }
                                             
     usleep (1000); 
+  }
+  
+  if (bruteF) {
+    bruteforce (&target);
   }
   
   for (int i = 0; i < threadCount; i++) {
